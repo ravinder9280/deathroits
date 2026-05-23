b567a40fe0f8cdef8bcbcb79603731aeffe5ad04
ravinder9280 <ravinder92809@gmail.com>
2026-05-23 22:21:26 +0530

    fix: nextjs middleware3

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,24 +1,18 @@
-// middleware.ts
 import { NextRequest, NextResponse } from "next/server";
+import { getSessionCookie } from "better-auth/cookies";
 
 export async function middleware(request: NextRequest) {
-	const response = await fetch(
-		`${process.env.NEXT_PUBLIC_API_ORIGIN}/api/auth/get-session`, {
-			headers: {
-				cookie: request.headers.get("cookie") || "",
-			},
-		}
-	);
 
-	if (!response.ok) {
-		return NextResponse.redirect(new URL("/sign-in", request.url));
-	}
+    // THIS IS NOT SECURE!
+ 
+    const sessionCookie = getSessionCookie(request, {
+      cookiePrefix: "better-auth",
+    });
 
-	const session = await response.json();
-
-	if (!session) {
-		return NextResponse.redirect(new URL("/sign-in", request.url));
-	}
 
 	return NextResponse.next();
 }
+
+export const config = {
+	matcher: ["/tournaments"], // Specify the routes the middleware applies to
+};
