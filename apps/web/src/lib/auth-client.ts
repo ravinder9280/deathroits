6a63dc6b5d60a427c6de2e9b617f4fa60e9c6168
ravinder9280 <ravinder92809@gmail.com>
2026-06-24 13:50:16 +0530

    feat: email flow2

--- apps/web/src/lib/auth-client.ts
+++ apps/web/src/lib/auth-client.ts
@@ -6,6 +6,8 @@ import { createAuthClient } from "better-auth/react";
 
 import { userAdditionalFields } from "../../../server/src/lib/auth-user-fields";
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore – better-auth's plugin generics produce a non-portable inferred type; suppressed intentionally
 const authClient = createAuthClient({
   baseURL: process.env.NEXT_PUBLIC_API_ORIGIN,
   basePath: '/api/auth',
